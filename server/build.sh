91b956da2e3ac05f34a4a3d2f09e9b967f60bbb5
Manish9026 <135260553+Manish9026@users.noreply.github.com>
2026-04-11 00:45:50 +0530

    Fix deployment dependencies and python version

--- server/build.sh
+++ server/build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 cd server
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
